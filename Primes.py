e7af4302a33d7e66acb9ba7326692dfc46f0cb12
Mohammed Alhassan <xxm37xx@gmail.com>
2022-02-15 16:26:07 +0300

    first commit

---
+++ Primes.py
@@ -0,0 +1,73 @@
+import time
+import sys
+
+def method_1(num):
+    numbers = set(range(num, 1, -1))    # all numbers in range (sorted backwards)
+    primes = []
+    start_time = time.time()
+    while numbers:
+        p = numbers.pop()   # p = the last element in the numbers list
+        primes.append(p)    # p add p to the primes list
+        numbers.difference_update(set(range(p*2, num+1, p))) # {numbers} - {(p*2 -> num+1) incremented by p}
+    execution_time = time.time() - start_time
+    total_numbers = len(primes)
+    print("\nresults: ")
+    print("The number of primes from 0 to %s is: %s " % (num, total_numbers))
+    print("The execution time is: %s seconds" % round(execution_time, 4))
+
+def method_2(num):
+    lower = 0
+    count = 0
+    start_time = time.time()
+    for n in range( lower, num + 1):
+        if n > 1:
+            for i in range(2, n):
+                if (n % i) == 0:
+                    break
+            else:
+                count +=1
+    execution_time = time.time() - start_time
+    total_numbers = count
+    print("\nresults: ")
+    print("The number of primes from 0 to %s is: %s " % (num, total_numbers))
+    print("The execution time is: %s seconds" % round(execution_time, 4))
+
+
+def CheckPrime(i,n):
+    if n==i:
+        return 0
+    else:
+        if(n%i==0):
+            return 1
+        else:
+            return CheckPrime(i+1,n)
+
+def method_3(num):
+    ("Number of primes are")
+    count = 0
+    start_time = time.time()
+    for i in range(2, num+1):
+     if(CheckPrime(2,i)==0):
+         count +=1
+    execution_time = time.time() - start_time
+    total_numbers = count
+    print("\nresults: ")
+    print("The number of primes from 0 to %s is: %s " % (num, total_numbers))
+    print("The execution time is: %s seconds" % round(execution_time, 4))
+
+while(True):
+    print("============================================================")
+    num = int(input("Enter a number: "))
+    print("\n1. using a stack (fast)\n2. using nested a loop (slow)\n3. using recursion (slowest) (MAX 20,000)")
+    option = input("\nChoose a method: ")
+    print("\ncalculating...")
+    if option == "1":
+       method_1(num)
+    elif option == "2":
+       method_2(num)
+    elif option == "3":
+        sys.setrecursionlimit(999999)
+        method_3(num)
+    else:
+       print("unknown input, please choose 1 or 2")
+print("============================================================")
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Algorithm-Efficiency-Test
+A small experiment I did with python to test different types of algorithm's effieciency. Algorithm efficiency plays a vital role in any program specially when dealing with Big Data.
+
+
+Most common Big Data applications:
+- `Goverment`: 
+Proccess efficiencies in terms of cost, productivity, and innovation without its flaws.
+- `Inernational development`:
+Research on the effective usage of information and communication technologies for development 
+- `Healthcare`:
+Track infected people to minimise spread.
+- `Education`
+- `Media`
+- `Incurance`
+
+    ...
+
+    and lots more
+
+## ⚙️ Technologies
+
+<img src="https://img.icons8.com/color/48/000000/python--v1.png"/>
+
+## ⚡ Features
+
+-   Allows the user to choose 3 different algorithms with different efficiecies.
+- Displays the execution time for each run.
+
+## 💻 Supported operations
+-   `stacks`: calculates the total number of prime numbers of a set using a single loop and a stack data structure.
+
+    Time complexity: O(n)
+
+    (most efficient)
+
+<br>
+
+- `nested loops`: calcualtes the total prime numbers using nested loops.
+
+    Time complexity: O(n)^2
+
+    (less efficient but more accurate)
+
+<br>
+
+- `recursion`: calculates the total prime numbers of a set by itrating throught the came function while incrementing the number each time.
+
+    (Least efficient)
+
+[Note: python supports a limited number of recursive calls, so large numbers do not always work]
+
+
+## 🔧 Installation
+
+1. Install newest version of [Python](https://www.python.org/)
+2. Run program
+
+## ✒️ Author
+
+| [<img src="https://github.com/M-Alhassan.png?size=115" width="115"><br><sub>@M-Alhassan</sub>](https://github.com/M-Alhassan) |
+| :---------------------------------------------------------------------------------------------------------------------------: |
